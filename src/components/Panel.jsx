ed2ad188ef183bfe7d8a51c4cd0dd6ed47f6096c
pooya.oa <pooya.oladazimi@tib.eu>
2022-08-17 16:58:53 +0200

    added tabs

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,5 +1,7 @@
 import Grid from '@material-ui/core/Grid';
 import Pic from '../pic.jpg';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
 
 export function panel(){
@@ -7,7 +9,20 @@ export function panel(){
         <Grid container>
             <Grid item xs={12} id="image-section">
                 <img src={Pic} className="img-rounded" alt="my Pic"  id="my-image" /> 
-            </Grid>        
+            </Grid>
+            <Grid item xs={12} id="menu-section">
+            <Tabs            
+                orientation="vertical"
+                indicatorColor="primary"              
+                aria-label="Vertical tabs example"
+                >
+                <Tab label="About Me"/>
+                <Tab label="Job Experience" />
+                <Tab label="Edjucation" />
+                <Tab label="Find Me"/>
+            </Tabs>                
+            </Grid>
+    
         </Grid>
     ];
 }
